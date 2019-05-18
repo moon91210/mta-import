99562a83c346003b99a022477ae623e2d8e2ca0d
moon91210 <smulders@protonmail.com>
2019-05-19 01:24:29 +0200

    more fixes

--- shared.lua
+++ shared.lua
@@ -7,7 +7,7 @@ function load()
 				error("Bad arg #1 to 'import' (string expected)", 3)
 			end
 
-			local content, err = exports[]]..thisResourceName..[[]:getFileContents(name)
+			local content, err = exports.]]..thisResourceName..[[:getFileContents(name)
 
 			if not content then
 				error(string.format("Can't import '%s' (%s)", name, err), 2)
@@ -19,19 +19,21 @@ function load()
 	]]
 end
 
-function getFileContents(path)	
+function getFileContents(path)
 	path = split(path, '/')
 
 	local modulePath = table.concat(path, '/', 2)
+	local ext = modulePath:sub(-4) == '.lua' and '' or '.lua'
 
 	if modulePath then
-		path = string.format(':%s/%s.lua', path[1], modulePath)
+		path = string.format(':%s/%s%s', path[1], modulePath, ext)
 	else
-		path = string.format('%s.lua', path[1])
+		path = string.format('%s%s', path[1], ext)
 	end
 	
 	if fileExists(path) then
 		local f = fileOpen(path)
+
 		if f then
 			local content = f:read(f.size)
 			f:close()
@@ -39,9 +41,9 @@ function getFileContents(path)
 			print(string.format('%s imported %s', sourceResource.name, path))
 
 			return content
-		else
-			return false, 'can\'t open file'
 		end
+		
+		return false, 'can\'t open file'
 	end
 
 	return false, 'file not found'
